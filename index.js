8b1fcc7bad7bb838e3e91f515b0049ade9b566d7
impartial-cache-1887 <marciafreeman2011@gmail.com>
2019-09-28 03:02:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 function add(a,b) {
-  return a + b
+  return parseInt(a + b)
 }
 
 function subtract(a,b) {
-  return a - b
+  return parseInt(a - b)
 }
 
 function multiply(a,b) {
-  return a * b
+  return parseInt(a * b)
 }
 
 function divide(a,b) {
-  return a / b
+  return parseInt(a / b)
 }
+
+
 
 function inc(n) {
   return parseInt(n++)
